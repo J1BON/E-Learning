ef2a0e3bbf316c12171bd79f0e72801219d0ef26
J1BON <jibanahammed8@gmail.com>
2021-10-05 18:02:27 +0600

    404 edit

--- src/Components/Notfound/Notfound.js
+++ src/Components/Notfound/Notfound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import './Notfound.css'
 
 const Notfound = () => {
     return (
         <div>
+            <Header></Header>
+            <div className='notFound'>
             <h1 className='text-center m-5 p-5'><span className='text-danger'>404.</span> Not Found</h1>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
